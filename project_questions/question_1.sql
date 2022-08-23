da39d186a16cdc68461a8c3d1493b1026404cb42
leontioso <leontioso@gmail.com>
2022-08-24 00:49:14 +0300

    Refinement enrichment answer to question 1

--- project_questions/question_1.sql
+++ project_questions/question_1.sql
@@ -1,4 +1,25 @@
-select t1.mean_temp, t1.median, t1.max_temp, t1.min_temp, t2.days_with_null, t5.days_with_nonull, 3652 - t2.days_with_null - t5.days_with_nonull days_not_included, t1.decade
+select 
+	t1.decade decade_temp, t1.mean_temp, t1.median, 
+	t1.max_temp, t1.min_temp, 
+	case
+		when (lead(t1.decade) over(order by t1.decade)) is not null then
+						case 
+							when t2.counted_days_temp is null then cast(concat(cast((lead(t1.decade) over(order by t1.decade)) as varchar), '0101') as date) - cast(concat(cast(t1.decade as varchar), '0101') as date)        
+							else cast(concat(cast((lead(t1.decade) over(order by t1.decade)) as varchar), '0101') as date) - cast(concat(cast(t1.decade as varchar), '0101') as date) - t2.counted_days_temp
+						end 
+		else null
+	end days_not_included_temp,
+	t3.decade decade_rr, t3.mean_rr, t3.median_rr, 
+	t3.max_rr, t3.min_rr, 
+	t3.decade decade_rr,
+	case
+		when (lead(t3.decade) over(order by t3.decade)) is not null then
+						case 
+							when t4.counted_days_rr is null then cast(concat(cast((lead(t3.decade) over(order by t3.decade)) as varchar), '0101') as date) - cast(concat(cast(t3.decade as varchar), '0101') as date)        
+							else cast(concat(cast((lead(t3.decade) over(order by t3.decade)) as varchar), '0101') as date) - cast(concat(cast(t3.decade as varchar), '0101') as date) - t4.counted_days_rr
+						end 
+		else null
+	end days_not_included_rr
 from
 (select 
 	avg(ebt.tg) mean_temp,
@@ -11,38 +32,33 @@ inner join stations s
 on ebt.staid = s.id and ebt.q_tg = 0 and s.cn='DE'
 group by div(extract(year from ebt.date), 10)) t1
 left join
-(select count(*) days_with_null, div(extract(year from dates_with_null.date_with_null), 10) * 10 decade
-from
-(select  date date_with_null from eca_blend_tg ebt2 
-inner join stations s2 
-on s2.id = ebt2.staid and cn = 'DE'
-where ebt2.tg is null or ebt2.q_tg != 0
-group by date) dates_with_null
-group by div(extract(year from dates_with_null.date_with_null), 10) * 10) t2
+(select count(distinct(ebt2.date)) counted_days_temp, div(extract(year from ebt2.date), 10) * 10 decade
+from eca_blend_tg ebt2
+inner join
+stations s2 on s2.id = ebt2.staid
+where s2.cn = 'DE' and ebt2.q_tg = 0
+group by div(extract(year from ebt2.date), 10)) t2
 on t1.decade = t2.decade
+full outer join
+(select 
+	avg(ebr.rr) mean_rr,
+	percentile_cont(0.5) within group(order by ebr.rr) median_rr, 
+	max(ebr.rr) max_rr, 
+	min(ebr.rr) min_rr,
+	(div(extract(year from ebr.date), 10)) * 10 decade
+from eca_blend_rr ebr
+inner join stations s 
+on ebr.staid = s.id and ebr.q_rr = 0 and s.cn='DE'
+group by div(extract(year from ebr.date), 10)) t3
+on t1.decade = t3.decade
 left join
-(select count(*) days_with_nonull, div(extract(year from t4.date_with_nonull), 10) * 10 as decade
-from
-(select t3.date_with_nonull from 
-(select  date date_with_nonull from eca_blend_tg ebt3 
-inner join stations s3 
-on s3.id = ebt3.staid and cn = 'DE'
-group by date) t3
-where t3.date_with_nonull not in
-(select  date date_with_null from eca_blend_tg ebt4 
-inner join stations s4 
-on s4.id = ebt4.staid and cn = 'DE'
-where ebt4.tg is null or ebt4.q_tg != 0
-group by date)) t4
-group by div(extract(year from t4.date_with_nonull), 10) * 10) t5
-on t1.decade = t5.decade
-
-select distinct(ebt.date) date_with_null from eca_blend_tg ebt 
-inner join sources s 
-on s.staid = ebt.staid and s.souid = ebt.souid and substring(s.elei from 1 for 2) = 'TG' and cn = 'DE'
-where ebt.tg is null
-
-
+(select count(distinct(ebr2.date)) counted_days_rr, div(extract(year from ebr2.date), 10) * 10 decade
+from eca_blend_rr ebr2
+inner join
+stations s3 on s3.id = ebr2.staid
+where s3.cn = 'DE' and ebr2.q_rr = 0
+group by div(extract(year from ebr2.date), 10)) t4
+on t3.decade = t4.decade
 
 
 
