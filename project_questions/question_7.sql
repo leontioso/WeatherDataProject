9a564f072410620defbc17f60533ebaaa82a9172
leontioso <leontioso@gmail.com>
2022-08-23 10:31:47 +0300

    Created and Solved question 7

---
+++ project_questions/question_7.sql
@@ -0,0 +1,13 @@
+select 
+	avg(ebr.rr) avg_rr,
+	max(ebr.rr) max_rr,
+	count(ebr.rr) counts,
+	count(distinct(s.id)) stations,
+	s.cn
+from eca_blend_rr ebr
+inner join stations s
+on s.id = ebr.staid 
+where ebr.q_rr = 0
+group by s.cn
+order by   stations desc, counts desc, avg_temp desc, max_rr desc
+
