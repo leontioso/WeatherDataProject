539918467a20db9864ff382a8bf5706fb9961b11
leontioso <leontioso@gmail.com>
2022-08-24 22:54:58 +0300

    renaming question_2alt to question_2. Previous question 2 not needed

--- project_questions/question_2.sql
+++ project_questions/question_2.sql
@@ -1,80 +1,91 @@
-select 3652 - t_valid_days.days_with_nonull days_with_nonvalid_value,t_valid_days.days_with_nonull, t_valid_days.decade decade_valid, t90per.decade decade90, t10per.decade decade10, t90per.total_counts_decade, t90per.cumm_counts cumm_counts_90, t90per.cumm_avg_temp cumm_avg_temp_90,
-		t10per.cumm_counts cumm_counts_10, t10per.cumm_avg_temp cumm_avg_temp_10
+select table_tg.*, table_rr.*
 from
-(select count(*) days_with_nonull, div(extract(year from t4.date_with_nonull), 10) * 10 as decade
+(select 
+	t2.value_90per_tg, t2.value_10per_tg, t3.days_missing_values days_missing_values_tg,
+	case 
+		when t2.decade is null then 3652
+		when t4.total_days_counted is null then cast(concat(cast((t2.decade +10) as varchar), '0101') as date) - cast(concat(cast((t2.decade) as varchar), '0101') as date) - t4.total_days_counted
+		else cast(concat(cast((t2.decade +10) as varchar), '0101') as date) - cast(concat(cast((t2.decade) as varchar), '0101') as date) - t4.total_days_counted
+	end days_not_included_tg,
+	t2.decade decade_tg
+	from
+/* 
+ To find the 90percentile and 10percentile I am going to use the percentile_cont() function as I did to find the median. First we create a table with the average values per day in Germany (t1) and then we are trying
+ to find the percentiles as we are grouping per decade(t2). 
+ */
+(select 
+	percentile_cont(0.9) within group (order by t1.day_temp) value_90per_tg,
+	percentile_cont(0.1) within group (order by t1.day_temp) value_10per_tg,
+	div(extract(year from t1.date), 10) * 10 decade
 from
-(select t3.date_with_nonull from 
-(select  date date_with_nonull from eca_blend_tg ebt3 
-inner join sources s3 
-on s3.staid = ebt3.staid and s3.souid = ebt3.souid and substring(s3.elei from 1 for 2) = 'TG' and cn = 'DE'
-group by date) t3
-where t3.date_with_nonull not in
-(select  date date_with_null from eca_blend_tg ebt4 
-inner join sources s4 
-on s4.staid = ebt4.staid and s4.souid = ebt4.souid and substring(s4.elei from 1 for 2) = 'TG' and cn = 'DE'
-where ebt4.tg is null or ebt4.q_tg != 0
-group by date)) t4
-group by div(extract(year from t4.date_with_nonull), 10) * 10) t_valid_days
+(select 
+	avg(ebt.tg) day_temp,
+	ebt.date
+from eca_blend_tg ebt 
+inner join stations s 
+on ebt.staid = s.id
+where s.cn = 'DE' and ebt.q_tg = 0
+group by ebt.date) t1
+group by div(extract(year from t1.date), 10) * 10) t2
+/*After t1 and t2 we select the days that have missing values per decade (t3) and the we join them with t2 with outer join in case that a decade doesn't exist in t2
+ */
 full outer join
-(select distinct on (t2.decade)
-t2.total_counts_decade, t2.cumm_counts, t2.cumm_percent, t2.cumm_avg_temp, t2.decade
-from
-(select 
-	t1.counts_of_temp_decade,
-	t1.tg,
-	t1.total_counts_decade,
-	t1.perc_decade,
-	t1.decade * 10 decade,
-	sum(t1.counts_of_temp_decade) over(partition by t1.decade order by t1.perc_decade desc rows between unbounded preceding and current row) cumm_counts,
-	sum(t1.perc_decade) over(partition by t1.decade order by t1.perc_decade desc rows between unbounded preceding and current row) cumm_percent,
-	sum(t1.tg) over(partition by t1.decade order by t1.perc_decade desc rows between unbounded preceding and current row)/
-	sum(t1.perc_decade) over(partition by t1.decade order by t1.perc_decade desc rows between unbounded preceding and current row) cumm_avg_temp
-from
-(select 
- count(ebt.tg) counts_of_temp_decade,
- sum(count(ebt.tg)) over(partition by div(extract(year from ebt.date), 10) order by div(extract(year from ebt.date), 10)) total_counts_decade,
- cast(count(ebt.tg) as real) / cast(sum(count(ebt.tg)) over(partition by div(extract(year from ebt.date), 10) order by div(extract(year from ebt.date), 10)) as real) perc_decade,
- ebt.tg, 
- div(extract(year from ebt.date), 10) decade
-from eca_blend_tg ebt
-inner join sources s 
-on ebt.staid = s.staid and ebt.souid = s.souid and substring(s.elei from 1 for 2) = 'TG' and ebt.q_tg = 0 and s.cn='DE'
-group by ebt.tg, div(extract(year from ebt.date), 10)
-order by decade, counts_of_temp_decade desc) t1) t2
-where t2.cumm_percent <= 0.9
-order by t2.decade, t2.cumm_percent desc) t90per
-on t_valid_days.decade = t90per.decade
+(select count(distinct(ebt2.date)) days_missing_values, div(extract(year from ebt2.date), 10) * 10 decade
+from eca_blend_tg ebt2 
+inner join stations s 
+on s.id = ebt2.staid
+where s.cn = 'DE'
+group by  div(extract(year from ebt2.date), 10)) t3
+on t2.decade = t3.decade
+/*When we finish with the days that have missing values we count all the dates that exist per decade (t4) to discover if there dates in a decade that they are not included*/
+inner join
+(select count(distinct(ebt3.date)) total_days_counted, div(extract(year from ebt3.date), 10) * 10 decade
+from eca_blend_tg ebt3
+inner join stations s
+on ebt3.staid = s.id
+where s.cn = 'DE'
+group by div(extract(year from ebt3.date), 10)) t4
+on t4.decade = t3.decade) table_tg
 full outer join
-(select distinct on (t2.decade)
-t2.total_counts_decade, t2.cumm_counts, t2.cumm_percent, t2.cumm_avg_temp,t2.decade
+(select 
+	t2.value_90per_rr, t2.value_10per_rr, t3.days_missing_values days_missing_values_rr,
+	case 
+		when t2.decade is null then 3652
+		when t4.total_days_counted is null then cast(concat(cast((t2.decade +10) as varchar), '0101') as date) - cast(concat(cast((t2.decade) as varchar), '0101') as date) - t4.total_days_counted
+		else cast(concat(cast((t2.decade +10) as varchar), '0101') as date) - cast(concat(cast((t2.decade) as varchar), '0101') as date) - t4.total_days_counted
+	end days_not_included_rr,
+	t2.decade decade_rr
+	from
+(select 
+	percentile_cont(0.9) within group (order by t1.day_rr) value_90per_rr,
+	percentile_cont(0.1) within group (order by t1.day_rr) value_10per_rr,
+	div(extract(year from t1.date), 10) * 10 decade
 from
 (select 
-	t1.counts_of_temp_decade,
-	t1.tg,
-	t1.total_counts_decade,
-	t1.perc_decade,
-	t1.decade * 10 decade,
-	sum(t1.counts_of_temp_decade) over(partition by t1.decade order by t1.perc_decade desc rows between unbounded preceding and current row) cumm_counts,
-	sum(t1.perc_decade) over(partition by t1.decade order by t1.perc_decade desc rows between unbounded preceding and current row) cumm_percent,
-	sum(t1.tg) over(partition by t1.decade order by t1.perc_decade desc rows between unbounded preceding and current row)/
-	sum(t1.perc_decade) over(partition by t1.decade order by t1.perc_decade desc rows between unbounded preceding and current row) cumm_avg_temp
-from
-(select 
- count(ebt.tg) counts_of_temp_decade,
- sum(count(ebt.tg)) over(partition by div(extract(year from ebt.date), 10) order by div(extract(year from ebt.date), 10)) total_counts_decade,
- cast(count(ebt.tg) as real) / cast(sum(count(ebt.tg)) over(partition by div(extract(year from ebt.date), 10) order by div(extract(year from ebt.date), 10)) as real) perc_decade,
- ebt.tg, 
- div(extract(year from ebt.date), 10) decade
-from eca_blend_tg ebt
-inner join sources s 
-on ebt.staid = s.staid and ebt.souid = s.souid and substring(s.elei from 1 for 2) = 'TG' and ebt.q_tg = 0 and s.cn='DE'
-group by ebt.tg, div(extract(year from ebt.date), 10)
-order by decade, counts_of_temp_decade desc) t1) t2
-where t2.cumm_percent <= 0.1
-order by t2.decade, t2.cumm_percent desc) t10per
-on t_valid_days.decade = t10per.decade
-
-
-
+	avg(ebr.rr) day_rr,
+	ebr.date
+from eca_blend_rr ebr 
+inner join stations s 
+on ebr.staid = s.id
+where s.cn = 'DE' and ebr.q_rr = 0
+group by ebr.date) t1
+group by div(extract(year from t1.date), 10) * 10) t2
+full outer join
+(select count(distinct(ebr2.date)) days_missing_values, div(extract(year from ebr2.date), 10) * 10 decade
+from eca_blend_rr ebr2 
+inner join stations s 
+on s.id = ebr2.staid
+where s.cn = 'DE' and ebr2.q_rr != 0
+group by  div(extract(year from ebr2.date), 10)) t3
+on t2.decade = t3.decade
+inner join
+(select count(distinct(ebr3.date)) total_days_counted, div(extract(year from ebr3.date), 10) * 10 decade
+from eca_blend_rr ebr3
+inner join stations s
+on ebr3.staid = s.id
+where s.cn = 'DE'
+group by div(extract(year from ebr3.date), 10)) t4
+on t4.decade = t3.decade) table_rr
+on table_tg.decade_tg = table_rr.decade_rr
 
 
