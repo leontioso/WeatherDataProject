575d60b4ae80a0f024e8d41b8fc410347d5935b3
leontioso <leontioso@gmail.com>
2022-08-25 12:36:34 +0300

    Commented the answer to question 4

--- project_questions/question_4.sql
+++ project_questions/question_4.sql
@@ -1,33 +1,35 @@
+/*
+First I create a table with the 90 percentile of the average temperature per decade for every country (t1).
+Then I use a subquery tha greturns me the average temperature per day for every country, the date, the
+decade and the country. After that I use an inner join to combine t1 and t2 on decade so that I can practically add an extra column
+to the t2 with the 90 percentile per decade per country. This table is t3. Then I group by the t3 per decade and country
+and I use a case to count how many dates are above the 90 percentile of the decade.
+*/
 select 
-count(t4.date) total_days,
+count(t3.date) total_days,
 sum(case 
-	when t4.temp_day > t4.percent90 then 1
+	when t3.temp_day > t3.percent90 then 1
 end) days_above_90per,
-t4.decade,
-t4.cn
-from
-(select
-t3.temp_day,
-t3.cn,
-t3.date,
 t3.decade,
-t1.percent90
-from 
+t3.cn
+from
 (select
 t2.temp_day,
 t2.cn,
 t2.date,
-div(extract(year from t2.date), 10) * 10 decade
-from
+t2.decade,
+t1.percent90
+from 
 (select 
 	avg(ebt.tg) temp_day,
 	ebt.date,
+	div(extract(year from t2.date), 10) * 10 decade,
 	s.cn
 from eca_blend_tg ebt 
 inner join stations s 
 on s.id = ebt.staid
 where ebt.q_tg = 0
-group by ebt.date, s.cn) t2) t3
+group by ebt.date, s.cn) t2
 inner join
 (select 
 	percentile_cont(0.9) within group (order by ebt.tg) percent90,
@@ -38,8 +40,8 @@ inner join stations s
 on s.id = ebt.staid
 where ebt.q_tg = 0
 group by decade, s.cn) t1
-on t1.decade = t3.decade and t1.cn = t3.cn) t4
-group by t4.cn, t4.decade
+on t1.decade = t2.decade and t1.cn = t2.cn) t3
+group by t3.cn, t3.decade
 
 
 
